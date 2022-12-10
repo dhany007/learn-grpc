396da6a73d6c8795156d0732f9dfccaf4dc3f6d0
dhanykoinworks <dhany.aritonang@koinworks.com>
2022-12-10 08:03:18 +0700

    primes streaming server implementing

--- calculator/client/calculator.go
+++ calculator/client/calculator.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 
 	pb "github.com/dhany007/learn-grpc/calculator/proto"
 )
 
+// unary implementing
 func doSum(c pb.CalculatorServiceClient) {
 	log.Println("doSum function was invoked")
 
@@ -20,4 +22,31 @@ func doSum(c pb.CalculatorServiceClient) {
 	}
 
 	log.Printf("SUM: %d", res.Result)
+}
+
+// server streaming implementing
+func doPrimes(c pb.CalculatorServiceClient) {
+	log.Println("doPrimes function was invoked")
+	req := &pb.PrimeRequest{
+		Number: 123513456,
+	}
+
+	stream, err := c.Primes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while stram: %+v", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("error while reading the stream: %+v", err)
+		}
+
+		log.Printf("Primes = %d\n", msg.Result)
+	}
 }
--- calculator/server/calculator.go
+++ calculator/server/calculator.go
@@ -7,10 +7,35 @@ import (
 	pb "github.com/dhany007/learn-grpc/calculator/proto"
 )
 
+// uanry implementing
 func (s *Server) Sum(ctx context.Context, in *pb.SumRequest) (*pb.SumResponse, error) {
 	log.Printf("Sum function was invoked with: %+v\n", in)
 
 	return &pb.SumResponse{
 		Result: in.FirstNumber + in.SecondNumber,
 	}, nil
+}
+
+// server streaming implementing
+func (s *Server) Primes(in *pb.PrimeRequest, stream pb.CalculatorService_PrimesServer) error {
+	log.Printf("Primes was invoked with: %+v\n", in)
+
+	var (
+		divisor int32 = 2
+		number  int32 = in.Number
+	)
+
+	for number > 1 {
+		if number%divisor == 0 {
+			stream.Send(&pb.PrimeResponse{
+				Result: divisor,
+			})
+
+			number = number / divisor
+		} else {
+			divisor += 1
+		}
+	}
+
+	return nil
 }
