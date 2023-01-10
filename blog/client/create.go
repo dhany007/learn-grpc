5d36b9637f451a2514ea3b5444dd1000d0f35560
dhanykoinworks <dhany.aritonang@koinworks.com>
2023-01-10 13:50:36 +0700

    CreateBlog Client

---
+++ blog/client/create.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/dhany007/learn-grpc/blog/proto"
+)
+
+func createBlog(c pb.BlogServiceClient) {
+	log.Println("--- createBlog was invoked ---")
+
+	blog := &pb.Blog{
+		AuthorId: "Dhany",
+		Title:    "My First Blog",
+		Content:  "Content of the first blog",
+	}
+
+	res, err := c.CreateBlog(context.Background(), blog)
+	if err != nil {
+		log.Fatalf("unexpected error: %+v", err)
+	}
+
+	log.Printf("Blog has beed created: %s\n", res.Id)
+}
---
+++ blog/client/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	pb "github.com/dhany007/learn-grpc/blog/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var addr string = "localhost:50052"
+
+func main() {
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("failed to connect: %+v\n", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewBlogServiceClient(conn)
+
+	createBlog(c)
+}
