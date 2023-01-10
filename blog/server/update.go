b6220aaac6e3614013f0ee4dd448acb5b5e7c5f2
dhanykoinworks <dhany.aritonang@koinworks.com>
2023-01-10 15:31:22 +0700

    UpdateBlog Server

---
+++ blog/server/update.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/dhany007/learn-grpc/blog/proto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (s *Server) UpdateBlog(ctx context.Context, in *pb.Blog) (*emptypb.Empty, error) {
+	log.Printf("function UpdateBlog was invoked with: %+v\n", in)
+
+	oid, err := primitive.ObjectIDFromHex(in.Id)
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"can not parse blog id",
+		)
+	}
+
+	data := &BlogItem{
+		AuthorId: in.AuthorId,
+		Title:    in.Title,
+		Content:  in.Content,
+	}
+
+	res, err := collection.UpdateOne(
+		ctx,
+		bson.M{"_id": oid},
+		bson.M{"$set": data},
+	)
+
+	if err != nil {
+		return nil, status.Errorf(
+			codes.Internal,
+			"could not update blog",
+		)
+	}
+
+	if res.MatchedCount == 0 {
+		return nil, status.Errorf(
+			codes.NotFound,
+			"could not find blog with id",
+		)
+	}
+
+	return &emptypb.Empty{}, nil
+}
